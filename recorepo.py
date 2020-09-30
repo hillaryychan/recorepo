38298ae4f96f3e7d605a69c302bce1ec012e58f2
Hillary Chan <hillary.chan91@gmail.com>
2020-09-30 21:25:25 +1000

    Try out flake8 linting

--- recorepo.py
+++ recorepo.py
@@ -11,6 +11,7 @@ def redirect_origin(path, ssh_url):
     subprocess.run(['git', '-C', path, 'remote', 'set-url', 'origin', ssh_url])
     subprocess.run(['git', '-C', path, 'push', '-u', 'origin', 'master'])
 
+
 def create_repo(token, name, init, private, https):
     print('Creating respository', name)
     r = requests.post('https://api.github.com/user/repos',
@@ -23,6 +24,7 @@ def create_repo(token, name, init, private, https):
         return response['html_url']
     return response['ssh_url']
 
+
 def main():
     for path in args.paths:
         repo_name = args.course_code + '-' + path
@@ -33,19 +35,24 @@ def main():
         except Exception as e:
             print(e)
 
+
 def course_code_type(value, pattern=re.compile(r'^[0-9]{4}$')):
     if not pattern.match(value):
         raise argparse.ArgumentTypeError('Course code {} is not a 4-digit code'.format(value))
     return value
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Redirect git repositories')
     parser.add_argument('token', help='GitHub OAuth token')
     parser.add_argument('course_code', type=course_code_type, help='A 4-digit course code')
     parser.add_argument('paths', nargs='+', help='Repositories to redirect')
-    parser.add_argument('-p', '--public', action='store_true', help='Make the repository public. Private by default')
-    parser.add_argument('-i', '--init', action='store_true', help='Initialise repository upon creation')
-    parser.add_argument('--https', action='store_true', help='Redirect repository to have HTTPS origin path. Uses SSH by default')
+    parser.add_argument('-p', '--public', action='store_true',
+                        help='Make the repository public. Private by default')
+    parser.add_argument('-i', '--init', action='store_true',
+                        help='Initialise repository upon creation')
+    parser.add_argument('--https', action='store_true',
+                        help='Redirect repository to have HTTPS origin path. Uses SSH by default')
 
     args = parser.parse_args()
     main()
